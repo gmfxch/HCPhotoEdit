31aadb10cb568fbce00356c47939ccfb3c3de753
陈浩1 <chenhao1@corp.hxqc.com>
2017-02-04 17:30:32 +0800

    Initial Commit

---
+++ HCPhotoEditDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCPhotoEditDemo
+//
+//  Created by chenhao on 17/2/4.
+//  Copyright © 2017年 chenhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCPhotoEditDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCPhotoEditDemo
+//
+//  Created by chenhao on 17/2/4.
+//  Copyright © 2017年 chenhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
